2704d50811531d5f4248adba4ae56d33063814c3
MathiasBergz <mathias15052004@gmail.com>
2026-04-12 15:06:56 -0300

    Implementação básica das threads

--- TDESO.c
+++ TDESO.c
@@ -4,7 +4,23 @@
 #include <unistd.h>
 #include <semaphore.h>
 
+#define NUM_THREADS 4
+
+void *readFile(void *path) {
+    printf("Read File function \n");
+    pthread_exit(NULL);
+}
+
 int main(int argc, char **argv) {
-    
+    pthread_t threads[NUM_THREADS];
+
+    for(int i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, &readFile, NULL);
+    }
+
+    for(int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
     return 0;
 }
