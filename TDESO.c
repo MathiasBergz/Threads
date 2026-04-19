00a710839595fb189657fb4b99705c618333162a
MathiasBergz <mathias15052004@gmail.com>
2026-04-19 16:33:38 -0300

    Criação da struct cidade e variaveis globais e implementação da leitura de arquivo

--- TDESO.c
+++ TDESO.c
@@ -6,13 +6,30 @@
 
 #define NUM_THREADS 4
 
+FILE *ftpr;
+float sumCXS;
+float sumBG;
+int contCXS;
+int contBG;
+
+typedef struct {
+    char name[30];
+    float maxTemp;
+    float minTemp;
+    float avgTemp;
+} City;
+
 void *readFile(void *path) {
-    printf("Read File function \n");
+    ftpr = fopen("", "r");
+    if(ftpr == NULL) {
+        printf("Arquivo nao encontrado.\n");
+    }
     pthread_exit(NULL);
 }
 
 int main(int argc, char **argv) {
     pthread_t threads[NUM_THREADS];
+    City cities[2];
 
     for(int i = 0; i < NUM_THREADS; i++) {
         pthread_create(&threads[i], NULL, &readFile, NULL);
